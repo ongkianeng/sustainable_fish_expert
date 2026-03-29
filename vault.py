6ea84ac16265bbe083411c91c67cdc3498e4b304
ongkianeng <ongkianeng@outlook.com>
2026-03-29 16:57:22 +0800

    Update vault.py

--- vault.py
+++ vault.py
@@ -113,7 +113,7 @@ def apply_memento(photo_file):
         
         # Calculate X and Y offsets to center the frame on the canvas
         frame_w, frame_h = frame_resized.size
-        x_offset = (TARGET_PRINT_SIZE - frame_w) // 2
+        x_offset = (TARGET_PRINT_SIZE - frame_w + 2) // 2
         y_offset = (TARGET_PRINT_SIZE - frame_h) // 2
         
         # Paste the frame using the calculated coordinates
